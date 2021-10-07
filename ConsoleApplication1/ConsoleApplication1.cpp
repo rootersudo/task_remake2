ece8c009c4ac8eeb149975bad022ff03ff01501d
Великодный Владимир Алексеевич <velikodnyij_v_a@cs.vsu.ru>
2021-10-07 10:25:40 +0300

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,36 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    while (true) {
+        int g = 0;
+        float a, b, c, a1, b1, c1, x, y;
+        
+        setlocale(0, "RU");
+        cout << "Введите все коэфициенты для уравнения прямой вида ax+by+c=0\n";
+
+        cin >> a >> b >> c;
+        cout << "Введите коэффициенты для второй прямой тогоже вида\n";
+
+        cin >> a1 >> b1 >> c1;
+        
+
+        if (((a * b1) == (b * a1)) == (a * c1)==(c*b1)) {
+            cout << "Прямые совпадают\n";
+            g = 1;
+        }
+        else if ((a * b1 == b * a1) && (a * c1 != c * a1))
+            cout << "Прямые параллельны\n";
+
+        else {
+            if ((a * b1 != a1 * b) && (g == 0)) {
+                y = -(c1 * a + c * a1) / (b * a + b1 * a);
+                x = -(b * y + c) / a;
+                cout << "Координаты точки пересечения: (" << x << ";" << y << ")\n";
+            };
+        }
+    }
+    return 0;
+}
